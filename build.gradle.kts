ffa7114b4d30de4c2223e2788866037089de3bc5
REgorov <eogorovromanv@hotmail.com>
2025-02-05 12:57:04 +0300

    QP parsed one page

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jsoup:jsoup:1.7.2")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+import org.jsoup.Jsoup
+
 fun main() {
 
+    val doc =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+
+    val searchClass = ".sc-2aegk7-2"
+    val quoteElements = doc.select(searchClass)
+    for (element in quoteElements) {
+        println(element.text())
+    }
 }
